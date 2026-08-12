b6caf14a20b5d90027da6d3372f00c376cb4aeff
Gremiger <santiago.gremiger@etendo.software>
2026-08-12 17:14:09 +0000

    Feature ETP-4852: Fix assertThrows import in QA integration test
    Checks: xml, junit, coverage
    Hooks-Verified: v2 36a32dfb 6ce799f18119 9cd781f8b589

--- src-test/src/com/etendoerp/go/roles/UserRoleCompositionServiceIntegrationTest.java
+++ src-test/src/com/etendoerp/go/roles/UserRoleCompositionServiceIntegrationTest.java
@@ -18,8 +18,8 @@ package com.etendoerp.go.roles;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.Arrays;
 import java.util.Collections;
